8d8c2d3903b45d378da3f9b3fe67e4781cc9be5d
Moises Guerola <daw.moisesguerola@gmail.com>
2022-10-02 20:05:01 +0200

    Backend - Started Slug Generator

--- backend/src/models/Product.model.js
+++ backend/src/models/Product.model.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
+const slug = require('mongoose-slug-generator');
+
 
 const ProductSchema = mongoose.Schema({
     name: String,
-    categories: [{ type: String }],
+    categories: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }],
     desciption: String,
+    slug: { type: String, slug: "name" },
     img_url: [{ type: String }],
     condition: String,
     owner: String
 }, {
-    timestamps: true
+    timestamps: true,
 })
+
+mongoose.plugin(slug);
+
+ProductSchema.pre('validate', function(next) {
+    if (!this.slug) throw new Error()
+    next();
+});
+
+
+ProductSchema.methods.slugifyProduct = () => {
+    this.slug = slugify(this.name)
+}
 
 module.exports = mongoose.model('product', ProductSchema)
