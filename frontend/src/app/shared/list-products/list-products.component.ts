447f30a3bcbe51a4d77e5eae884b6044da12aa35
Moises Guerola <daw.moisesguerola@gmail.com>
2022-10-28 13:40:34 +0200

    Frontend - ProductList in Dev

--- frontend/src/app/shared/list-products/list-products.component.ts
+++ frontend/src/app/shared/list-products/list-products.component.ts
@@ -2,36 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Category, Product } from 'src/app/core/models';
 
-
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.css']
+  styleUrls: ['./list-products.component.css'],
 })
 export class ListProductsComponent implements OnInit {
-  filters?: any
-  products?: Product[]
+  filters?: any;
+  products!: Product[];
 
-  allCategories?: Category[]
+  allCategories?: Category[];
 
-  constructor(
-    private aRouter: ActivatedRoute,
-    private router: Router
-  ) {
-    this.aRouter.queryParams.subscribe(res => {
-      this.filters = res
-    })
+  constructor(private aRouter: ActivatedRoute, private router: Router) {
+    this.aRouter.queryParams.subscribe((res) => {
+      this.filters = res;
+    });
   }
 
   setProducts(e: any) {
-    this.products = e
+    this.products = e;
   }
 
   redirect(id?: String) {
-    this.router.navigateByUrl(`shop/product/${id}`)
+    this.router.navigateByUrl(`shop/product/${id}`);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
